5719f9c7c93c58748c2ae0c2441f188cac295c79
emilied82 <emiliedidier82@gmail.com>
2017-05-01 20:49:26 +0200

    Fixed issue on graphs

--- src/pages/graphs/graphs.ts
+++ src/pages/graphs/graphs.ts
@@ -36,68 +36,64 @@ public muscleRateDataEntries = [];
 public fatRateDataLabels = [];
 public fatRateDataEntries = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public dataService: Data) {    
-    this.dataService.getData().then((entries) => { 
-      if(entries){
-        let items = [];      
-        items = JSON.parse(entries);
-        for (let item of items) {         
-            console.log("date : "+item.date + " / poids : "+item.weight);// Fonctionne et retourne "hici : [object Object]          
-            this.weightDataLabels.push(item.date);
-            this.weightDataEntries.push(item.weight);
-            console.log("longueur weightDataEntries: "+this.weightDataEntries.length);
-            console.log("longueur weightDataLabels: "+this.weightDataLabels.length);
-            if(item.imc != null){
-                this.imcDataLabels.push(item.date);
-                this.imcDataEntries.push(item.imc);
-            }
-            if(item.muscleRate != null){
-                this.muscleRateDataLabels.push(item.date);
-                this.muscleRateDataEntries.push(item.muscleRate);
-            }
-            if(item.fatRate != null){
-                this.fatRateDataLabels.push(item.date);
-                this.fatRateDataEntries.push(item.fatRate);
-            }
-        }          
-      }
-    });   
+  constructor(public navCtrl: NavController, public navParams: NavParams, public dataService: Data) {       
+       
   }
 
-  ionViewDidLoad() {  
-
-    // TODO: exécuter ce code seulement si items not null. Else: message d'erreur
-    this.lineChart = new Chart(this.lineCanvas.nativeElement, { 
-        type: 'line',
-        data: {
-            labels: this.weightDataLabels,
-            datasets: [
-                {
-                    label: "Weight evolution",
-                    fill: true, // false or true
-                    lineTension: 0.1,
-                    backgroundColor: "rgba(75,192,192,0.4)",
-                    borderColor: "rgba(75,192,192,1)",
-                    borderCapStyle: 'butt',
-                    borderDash: [],
-                    borderDashOffset: 0.0,
-                    borderJoinStyle: 'miter',
-                    pointBorderColor: "#FF1654",
-                    pointBackgroundColor: "#fff",
-                    pointBorderWidth: 1,
-                    pointHoverRadius: 5,
-                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                    pointHoverBorderColor: "rgba(220,220,220,1)",
-                    pointHoverBorderWidth: 2,
-                    pointRadius: 2, // 1
-                    pointHitRadius: 10,
-                    data: this.weightDataEntries,
-                    spanGaps: false,
+  ionViewDidLoad() {
+      this.dataService.getData().then((entries) => { 
+        if(entries){
+            let items = [];      
+            items = JSON.parse(entries);
+            for (let item of items) {         
+                console.log("date : "+item.date + " / poids : "+item.weight);// Fonctionne et retourne "hici : [object Object]          
+                this.weightDataLabels.push(item.date);
+                this.weightDataEntries.push(item.weight);            
+                if(item.imc != null){
+                    this.imcDataLabels.push(item.date);
+                    this.imcDataEntries.push(item.imc);
                 }
-            ]
-        }
-       });       
-       this.lineChartImc = new Chart(this.lineCanvasImc.nativeElement, { 
+                if(item.muscleRate != null){
+                    this.muscleRateDataLabels.push(item.date);
+                    this.muscleRateDataEntries.push(item.muscleRate);
+                }
+                if(item.fatRate != null){
+                    this.fatRateDataLabels.push(item.date);
+                    this.fatRateDataEntries.push(item.fatRate);
+                }
+            }
+            // TODO: exécuter ce code seulement si items not null. Else: message d'erreur
+            this.lineChart = new Chart(this.lineCanvas.nativeElement, { 
+                type: 'line',
+                data: {
+                    labels: this.weightDataLabels,
+                    datasets: [
+                        {
+                            label: "Weight evolution",
+                            fill: true, // false or true
+                            lineTension: 0.1,
+                            backgroundColor: "rgba(75,192,192,0.4)",
+                            borderColor: "rgba(75,192,192,1)",
+                            borderCapStyle: 'butt',
+                            borderDash: [],
+                            borderDashOffset: 0.0,
+                            borderJoinStyle: 'miter',
+                            pointBorderColor: "#FF1654",
+                            pointBackgroundColor: "#fff",
+                            pointBorderWidth: 1,
+                            pointHoverRadius: 5,
+                            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                            pointHoverBorderColor: "rgba(220,220,220,1)",
+                            pointHoverBorderWidth: 2,
+                            pointRadius: 2, // 1
+                            pointHitRadius: 10,
+                            data: this.weightDataEntries,
+                            spanGaps: false,
+                        }
+                    ]
+                }
+               });
+               this.lineChartImc = new Chart(this.lineCanvasImc.nativeElement, { 
         type: 'line',
         data: {
             labels: this.imcDataLabels,
@@ -188,7 +184,10 @@ public fatRateDataEntries = [];
                 }
             ]
         }
-       });     
+       });                
+        }
+    });        
+            
   }
 
 }

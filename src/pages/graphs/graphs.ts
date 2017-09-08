cd970e771410c6a8a15f3b6fc6c564e5423e6a5d
emilied82 <emiliedidier82@gmail.com>
2017-09-08 21:17:16 +0200

    fixed issue of graphs not updating after first loading

--- src/pages/graphs/graphs.ts
+++ src/pages/graphs/graphs.ts
@@ -40,8 +40,55 @@ public fatRateDataEntries = [];
        
   }
 
-  ionViewDidLoad() {
-      this.dataService.getData().then((entries) => { 
+ ionViewDidEnter(){
+    // Si la vue est réaffichée, on vide tous les graphs pour les remettre à jour
+    this.lineChart = null;
+    this.weightDataLabels = [];
+    this.weightDataEntries = [];
+
+    this.dataService.getData().then((entries) => { 
+         if(entries){
+            let items = [];      
+            items = JSON.parse(entries);
+            for (let item of items) {         
+                this.weightDataLabels.push(item.date);
+                this.weightDataEntries.push(item.weight);     
+            }
+            this.lineChart = new Chart(this.lineCanvas.nativeElement, { 
+                type: 'line',
+                data: {
+                    labels: this.weightDataLabels,
+                    datasets: [
+                        {
+                            label: "Weight evolution",
+                            fill: true, // false or true
+                            lineTension: 0.1,
+                            backgroundColor: "rgba(75,192,192,0.4)",
+                            borderColor: "rgba(75,192,192,1)",
+                            borderCapStyle: 'butt',
+                            borderDash: [],
+                            borderDashOffset: 0.0,
+                            borderJoinStyle: 'miter',
+                            pointBorderColor: "#FF1654",
+                            pointBackgroundColor: "#fff",
+                            pointBorderWidth: 1,
+                            pointHoverRadius: 5,
+                            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                            pointHoverBorderColor: "rgba(220,220,220,1)",
+                            pointHoverBorderWidth: 2,
+                            pointRadius: 2, // 1
+                            pointHitRadius: 10,
+                            data: this.weightDataEntries,
+                            spanGaps: false,
+                        }
+                    ]
+                }
+            });
+        }
+    });
+}
+ionViewDidLoad() {
+    this.dataService.getData().then((entries) => { 
         if(entries){
             let items = [];      
             items = JSON.parse(entries);
@@ -189,5 +236,4 @@ public fatRateDataEntries = [];
     });        
             
   }
-
 }

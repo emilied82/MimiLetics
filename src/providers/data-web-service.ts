bb5154494e1e768e179ad3c1d45bbb256adaa8b0
emilied82 <emiliedidier82@gmail.com>
2018-04-11 22:48:32 +0200

    WOUWOUWOUH ! La liste est rÃcupÃrÃe dynamiquement du Web Service, l'ajout fonctionne aussi ! 	modified:   src/providers/data-web-service.ts

--- src/providers/data-web-service.ts
+++ src/providers/data-web-service.ts
@@ -10,45 +10,82 @@ import 'rxjs/add/operator/map';
   See https://angular.io/docs/ts/latest/guide/dependency-injection.html
   for more info on providers and Angular 2 DI.
 */
+
 @Injectable()
 export class DataWebService {
-	 public data: any;
+	 public data: any;	 
 
   constructor(public http: Http) {
     console.log('Hello DataWebService Provider');
   }
 
   load() {
-  	
-    let headers = new Headers();     
+
+  	/*let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    headers.append("Content-Type", 'application/json'); 
+    //headers.append("Access-Control-Allow-Methods", 'POST, GET, OPTIONS, PUT');*/
+
+    let headers = new Headers(); 
     headers.append("Access-Control-Allow-Origin" , 'http://localhost:8100');    
     headers.append("Content-Type", 'application/x-www-form-urlencoded');
-    let requestOptions = new RequestOptions({ headers:headers,withCredentials: true});
-    
+
+    let requestOptions = new RequestOptions({ headers:headers,withCredentials: true});    
 	  if (this.data) {
 	    // already loaded data	    
 	    console.log("data-web-service: already loaded data"+this.data);
 	    return Promise.resolve(this.data);
 	  }
-
 	  // don't have the data yet
-	  return new Promise((resolve, reject) => {	  	
-	    // We're using Angular HTTP provider to request the data,
-	    // then on the response, it'll map the JSON data to a parsed JS object.
-	    // Next, we process the data and resolve the promise with the new data	 
+	  return new Promise((resolve, reject) => {		   
+	    // post(url: string, body: any, options?: RequestOptionsArgs)
 	    //this.http.get('https://randomuser.me/api/?results=10')
-	      this.http.post('http://localhost/fitness-app/api.php?rquest=getdata', '{"userid": 1}', requestOptions )
+	      this.http.post('http://localhost/fitness-app/api.php?rquest=getdata', '{"userid": 2}', requestOptions )
 	      .map(res => res.json())
 	      .subscribe(data => {
-			this.data = data;			
+			this.data = data;
+			console.log("data-web-service: don't have the data yet "+data);
 			resolve(this.data);
 		}, err => reject(err));
 	  });
 	}
 
-	addEntry(item) {  	
-    
-	}
+	 addEntry(item: any) {
+	 	console.log("entering add entry"+item.weight);
+	 	let weight = item.weight; // obligatoire
+	 	let date = "2018-04-11 00:00:00"; // obligatoire 
+        let userid = item.userid; // obligatoire
+        let imc =  item.imc;
+        let muscleRate =  item.musclerate;
+        let fatRate = item.fatrate;
+        // ajouter visceral fat level & metabolism
+       
+        let headers = new Headers(); 
+    	headers.append("Access-Control-Allow-Origin" , 'http://localhost:8100');    
+    	headers.append("Content-Type", 'application/x-www-form-urlencoded');
 
+	    let requestOptions = new RequestOptions({ headers:headers,withCredentials: true});
+		let body = '{ "weight": '+weight+',  "userid": '+userid+', "date_time": "'+date+'"}';
+		/*if (imc != null){ body += ", 'imc': "+imc}
+		if (muscleRate != null){ body += ", 'muscleRate': "+muscleRate}
+		body += "}";*/		
 
+		/*let formData = new FormData();
+	      formData.append("weight", weight);
+	      formData.append("userid", userid);
+	      formData.append("datetime", date);*/
+     
+
+		//imc:"+imc+"musclerate: "+muscleRate+",
+    	
+	  	return this.http.post('http://localhost/fitness-app/api.php?rquest=postdata', body, requestOptions) .map(res => res/*.json()*/).subscribe(
+          data => {
+            console.log(data);
+          },
+          err => {
+            console.log("ERROR!: ", err);
+          }
+      );   
+
+ 	}
 }
